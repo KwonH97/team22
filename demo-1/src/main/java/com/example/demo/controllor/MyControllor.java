4149aeb309214af8be4140931e5363ada0bdce05
Jisun Kim <wltjs5963@naver.com>
2024-05-01 15:44:42 +0900

    Revert "컨트롤러, 서비스 업로드"
    This reverts commit 36c35e41f8d69341b1379e93df2812b1f7c30579.

--- demo-1/src/main/java/com/example/demo/controllor/MyControllor.java
+++ demo-1/src/main/java/com/example/demo/controllor/MyControllor.java
@@ -1,64 +1,13 @@
 package com.example.demo.controllor;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.example.demo.dto.Board;
-import com.example.demo.service.BoardService;
-
 
 @Controller
 public class MyControllor {
-	
-	@Autowired
-	private BoardService service;
-	
 	@RequestMapping("/")
 	public String root() {
-		return "main";
-	}
-	
-	@RequestMapping("/list")
-	public String listView(Model model) {
-		List<Board> list = service.getAllList();
-		model.addAttribute("list", list);
 		
-		return "list";
+		return "index";
 	}
-	
-	
-	@RequestMapping("/detail")
-	public String boardView(@RequestParam("bno") int bno, Model model) {
-		Board board = service.getBoard(bno);
-		model.addAttribute("board", board);
-		
-		return "detail";
-	}
-	
-	@RequestMapping("/regForm")
-	public String regForm() {
-		return "regForm";
-	}
-	
-	@RequestMapping("/regist")
-	public String regist(@RequestParam("title")String title, @RequestParam("content")String content, @RequestParam("writer")String writer) {
-		
-		Board board = new Board(0, title, content, writer);
-		service.regBoard(board);
-		
-		return "redirect:list";
-	}
-	
-	@RequestMapping("/delete")
-	public String delete(@RequestParam("bno")int bno) {
-		
-		service.deleteBoard(bno);
-		return "redirect:list";
-	}
-	
 }
